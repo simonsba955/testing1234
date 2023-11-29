6f57df1defe789c08e08568c5648be55c4b5b150
bengal865 <69059209+bengal865@users.noreply.github.com>
2023-11-29 10:41:24 -0500

    Add files via upload

---
+++ variables-demo.js
@@ -0,0 +1,9 @@
+// Mike Jenkins
+// 04 AUG 20XX
+// Variables Demo
+
+// Declare and initialize two variables 
+let firstName = 'Ethan';
+let age = 17;
+
+console.log(`My name is ${firstName} and I am ${age} years old.`);
